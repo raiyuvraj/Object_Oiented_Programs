014edac1830a163441f876719f098d762bf8a1bb
raiyuvraj <yuvirai017@gmail.com>
2023-01-17 17:38:34 +0530

    [Yuvraj-Invetory Management

---
+++ Object_Oriented_Programs/JsonInventoryMain.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programs
+{
+    internal class JsonInventoryMain
+    {
+        InventoryModel model = new InventoryModel();
+
+        List<InventoryModel> list = new List<InventoryModel>();
+
+        public void Convert(string jFilePath)
+        {
+            using (StreamReader file = new StreamReader(jFilePath))
+            {
+                var json = file.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<InventoryModel>>(json);
+                Console.WriteLine("Items Price Weight TotalValue");
+                foreach (var objects in items)
+                {
+                    double value = objects.price * objects.weight;
+                    Console.WriteLine(objects.name + " -> " + objects.price + " -> " + objects.weight + " -> " + value);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Object_Oriented_Programs/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+//JsonInventoryMain Json = new JsonInventoryMain();
+Object_Oriented_Programs.JsonInventoryMain Json = new Object_Oriented_Programs.JsonInventoryMain();
+
+Json.Convert("D:\\RFP-244\\ObjectOrientedPrograms\\Object_Oiented_Programs\\Object_Oriented_Programs\\jsconfig1.json");
